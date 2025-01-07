07a374198b43dd9e22e4c4482d806901ac6d4372
SukunaRaven <sabrinagenegel@gmail.com>
2025-01-07 11:01:02 +0100

    Start HTML

--- aanbod.php
+++ aanbod.php
@@ -1,1 +1,21 @@
-<?php
+<?php ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+    >
+    <link rel="stylesheet" href="style.css">
+    <title>Broers Smaakmakers</title>
+</head>
+<body>
+
+</body>
+</html>
+
