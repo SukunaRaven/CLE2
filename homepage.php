7ea6237cac059191483655ca368d2ce79d0c8fad
lisa-mao <1091575@hr.nl>
2025-01-07 14:48:46 +0100

    footer

--- homepage.php
+++ homepage.php
@@ -12,6 +12,7 @@
             href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
     >
     <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Broers Smaakmakers</title>
 </head>
 
