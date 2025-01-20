4f77aadf5e341d41f3a3cfff3625f4a9670cb540
Anemoia <killboy1100@gmail.com>
2025-01-20 14:54:22 +0100

    Fixed the nav

--- reserveringen.php
+++ reserveringen.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if (isset($_POST['submit'])) {
     /** @var mysqli $db */
     require_once "connection/connection.php";
@@ -84,14 +85,110 @@ if (isset($_POST['submit'])) {
     <link href="https://fonts.cdnfonts.com/css/imagination-station" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/open-sans" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"
+            type="text/javascript"></script>
     <title>Broers Smaakmakers</title>
 </head>
 
 <body>
 
-<?php include "./Nav/nav.php" ?>
+<nav class="navbar has-background-info" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item">
+            <figure class="image is-32x32">
+                <a href="../homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
+                                                              alt="Logo"/></a>
+            </figure>
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="nav-bar">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <script>
+                document.addEventListener('DOMContentLoaded', () => {
+
+                    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+                    $navbarBurgers.forEach(el => {
+                        el.addEventListener('click', () => {
+
+                            const target = el.dataset.target;
+                            const $target = document.getElementById(target);
+
+                            el.classList.toggle('is-active');
+                            $target.classList.toggle('is-active');
+
+                        });
+                    });
+
+                });
+            </script>
+        </a>
+    </div>
+
+    <div class="navbar-menu" id="nav-bar">
+        <div class="navbar-end">
+            <a href="../menu.php" class="navbar-item is-link">
+                Ons Menu
+            </a>
+
+            <a href="../reserveringen.php" class="navbar-item is-link">
+                Reserveer
+            </a>
+
+            <a href="../contact.php" class="navbar-item is-link">
+                Contact
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Meer
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a href="../aanbod.php" class="navbar-item">
+                        Ons Aanbod
+                    </a>
+                    <a href="../evenementen.php" class="navbar-item">
+                        Onze Evenementen
+                    </a>
+                    <a href="../arrangementen.php" class="navbar-item">
+                        Onze Arrangementen
+                    </a>
+                    <a href="../reviewspage.php" class="navbar-item">
+                        Reviews
+                    </a>
+                </div>
+            </div>
+        </div>
+        <?php if (empty($_SESSION)) : ?>
+            <div class="buttons">
+                <a href="../login.php" class="button is-primary is-outlined">
+                    Log In
+                </a>
+            </div>
+        <?php elseif (isset($_SESSION['admin_flag'])): ?>
+            <?php if ($_SESSION['admin_flag'] == 1): ?>
+                <div class="buttons">
+                    <a href="../admin.php" class="button is-primary is-outlined">
+                        Admin
+                    </a>
+                </div>
+            <?php endif; ?>
+        <?php else: ?>
+            <div class="buttons">
+                <a href="../logout.php" class="button is-link is-outlined">
+                    Log Out
+                </a>
+            </div>
+        <?php endif; ?>
+
+    </div>
+
+</nav>
 
 <header class="background-image is-primary is-medium">
     <section class="hero is-height">
