cb81c9683c2928d02ad43c9d051e7a3d7d2a9eab
Anemoia <killboy1100@gmail.com>
2025-01-20 14:53:09 +0100

    Fixed the nav

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if (isset($_POST['submit'])) {
     /** @var mysqli $db */
     require_once "connection/connection.php";
@@ -88,7 +89,102 @@ if (isset($_POST['submit'])) {
 
 <body>
 
-<?php include "./Nav/nav.php" ?>
+<nav class="navbar has-background-info" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item">
+            <figure class="image is-32x32">
+                <a href="../homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
+                                                              alt="Logo"/></a>
+            </figure>
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="nav-bar">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <script>
+                document.addEventListener('DOMContentLoaded', () => {
+
+                    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+                    $navbarBurgers.forEach(el => {
+                        el.addEventListener('click', () => {
+
+                            const target = el.dataset.target;
+                            const $target = document.getElementById(target);
+
+                            el.classList.toggle('is-active');
+                            $target.classList.toggle('is-active');
+
+                        });
+                    });
+
+                });
+            </script>
+        </a>
+    </div>
+
+    <div class="navbar-menu" id="nav-bar">
+        <div class="navbar-end">
+            <a href="../menu.php" class="navbar-item is-link">
+                Ons Menu
+            </a>
+
+            <a href="../reserveringen.php" class="navbar-item is-link">
+                Reserveer
+            </a>
+
+            <a href="../contact.php" class="navbar-item is-link">
+                Contact
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Meer
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a href="../aanbod.php" class="navbar-item">
+                        Ons Aanbod
+                    </a>
+                    <a href="../evenementen.php" class="navbar-item">
+                        Onze Evenementen
+                    </a>
+                    <a href="../arrangementen.php" class="navbar-item">
+                        Onze Arrangementen
+                    </a>
+                    <a href="../reviewspage.php" class="navbar-item">
+                        Reviews
+                    </a>
+                </div>
+            </div>
+        </div>
+        <?php if (empty($_SESSION)) : ?>
+            <div class="buttons">
+                <a href="../login.php" class="button is-primary is-outlined">
+                    Log In
+                </a>
+            </div>
+        <?php elseif (isset($_SESSION['admin_flag'])): ?>
+            <?php if ($_SESSION['admin_flag'] == 1): ?>
+                <div class="buttons">
+                    <a href="../admin.php" class="button is-primary is-outlined">
+                        Admin
+                    </a>
+                </div>
+            <?php endif; ?>
+        <?php else: ?>
+            <div class="buttons">
+                <a href="../logout.php" class="button is-link is-outlined">
+                    Log Out
+                </a>
+            </div>
+        <?php endif; ?>
+
+    </div>
+
+</nav>
 
 <header class="background-image is-primary is-medium">
     <section class="hero is-height">
@@ -106,7 +202,7 @@ if (isset($_POST['submit'])) {
 
             <form class="column is-6 container" action="" method="post">
 
-                <p style="color: #DF5F2C"    > Vul dit formulier in om een account aan te maken</p>
+                <p style="color: #DF5F2C"> Vul dit formulier in om een account aan te maken</p>
                 <!-- name -->
                 <div>
                     <label class="label" for="name">Voornaam</label>
@@ -122,13 +218,13 @@ if (isset($_POST['submit'])) {
                 </div>
                 <!-- surname -->
 
-<!--                <label class="label" for="surname">Achternaam</label>-->
-<!---->
-<!--                <input class="input is-link" placeholder="Achternaam" id="surname" type="text" name="surname"/>-->
-<!---->
-<!--                <p class="help is-danger">-->
-<!--                    --><?php //= $errors['surname'] ?? '' ?>
-<!--                </p>-->
+                <!--                <label class="label" for="surname">Achternaam</label>-->
+                <!---->
+                <!--                <input class="input is-link" placeholder="Achternaam" id="surname" type="text" name="surname"/>-->
+                <!---->
+                <!--                <p class="help is-danger">-->
+                <!--                    --><?php //= $errors['surname'] ?? '' ?>
+                <!--                </p>-->
 
                 <!--email -->
 
@@ -147,7 +243,8 @@ if (isset($_POST['submit'])) {
                 <label class="label" for="phoneNumber">Telefoonnummer</label>
 
 
-                <input class="input is-link" placeholder="Telefoonnummer" id="phoneNumber" type="text" name="phoneNumber"
+                <input class="input is-link" placeholder="Telefoonnummer" id="phoneNumber" type="text"
+                       name="phoneNumber"
                        value="<?= $phonenumber ?? '' ?>"/>
 
                 <p class="help is-danger">
@@ -178,9 +275,9 @@ if (isset($_POST['submit'])) {
                 <div class="privacyCheckBox">
                     <input class="checkbox" type="checkbox"/>
                     <div class="privacyText&Link">
-                    <p class="privacyText">Bij het aanmaken van een account geef je
-                        toestemming voor de <a class="linkPrivacy" href="#">Algemene Voorwaarden en
-                            Privacyverklaring</a></p>
+                        <p class="privacyText">Bij het aanmaken van een account geef je
+                            toestemming voor de <a class="linkPrivacy" href="#">Algemene Voorwaarden en
+                                Privacyverklaring</a></p>
                     </div>
                     <p class="help is-danger">
                         <?= $errors['privacy-verklaring'] ?? '' ?>
@@ -218,10 +315,11 @@ if (isset($_POST['submit'])) {
         font-size: smaller;
     }
 
-    .privacyText&Link {
+    .privacyText& Link {
         display: flex;
         align-content: center;
     }
+
     .checkbox {
         display: flex;
         transform: scale(1);
@@ -243,7 +341,7 @@ if (isset($_POST['submit'])) {
             </a>
 
             <a href="https://www.facebook.com/profile.php?id=61562490741128">
-                <i class="fa fa-facebook-square" style="font-size:30px" ></i>
+                <i class="fa fa-facebook-square" style="font-size:30px"></i>
             </a>
         </div>
         <div>
