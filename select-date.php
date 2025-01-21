efa0bc4cadf03e101500786760a88ec4da842a67
Anemoia <killboy1100@gmail.com>
2025-01-21 09:40:21 +0100

    New reserveringsysteem implemented

---
+++ select-date.php
@@ -0,0 +1,201 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css"
+    >
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Broers Smaakmakers</title>
+</head>
+
+<body>
+
+<nav class="navbar has-background-info" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item">
+            <figure class="image is-32x32">
+                <a href="../homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
+                                                              alt="Logo"/></a>
+            </figure>
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="nav-bar">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <script>
+                document.addEventListener('DOMContentLoaded', () => {
+
+                    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+                    $navbarBurgers.forEach(el => {
+                        el.addEventListener('click', () => {
+
+                            const target = el.dataset.target;
+                            const $target = document.getElementById(target);
+
+                            el.classList.toggle('is-active');
+                            $target.classList.toggle('is-active');
+
+                        });
+                    });
+
+                });
+            </script>
+        </a>
+    </div>
+
+    <div class="navbar-menu" id="nav-bar">
+        <div class="navbar-end">
+            <a href="../menu.php" class="navbar-item is-link">
+                Ons Menu
+            </a>
+
+            <a href="../reserveringen.php" class="navbar-item is-link">
+                Reserveer
+            </a>
+
+            <a href="../contact.php" class="navbar-item is-link">
+                Contact
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Meer
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a href="../aanbod.php" class="navbar-item">
+                        Ons Aanbod
+                    </a>
+                    <a href="../evenementen.php" class="navbar-item">
+                        Onze Evenementen
+                    </a>
+                    <a href="../arrangementen.php" class="navbar-item">
+                        Onze Arrangementen
+                    </a>
+                    <a href="../reviewspage.php" class="navbar-item">
+                        Reviews
+                    </a>
+                </div>
+            </div>
+        </div>
+        <?php if (empty($_SESSION)) : ?>
+            <div class="buttons">
+                <a href="../login.php" class="button is-primary is-outlined">
+                    Log In
+                </a>
+            </div>
+        <?php elseif (isset($_SESSION['admin_flag'])): ?>
+            <?php if ($_SESSION['admin_flag'] == 1): ?>
+                <div class="buttons">
+                    <a href="../admin.php" class="button is-primary is-outlined">
+                        Admin
+                    </a>
+                </div>
+            <?php endif; ?>
+        <?php else: ?>
+            <div class="buttons">
+                <a href="../logout.php" class="button is-link is-outlined">
+                    Log Out
+                </a>
+            </div>
+        <?php endif; ?>
+
+    </div>
+
+</nav>
+
+<header class="background-image is-primary is-medium">
+    <section class="hero is-height">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title is-1 shadow has-text-link">Reserveren</h1>
+            </div>
+        </div>
+    </section>
+</header>
+
+<main class="has-background-info pt-0">
+
+    <div class="container content is-flex is-justify-content-center">
+        <section class="columns">
+            <form class="column is-full" action="reserveringen.php" method="get">
+                <div class="pt-5"></div>
+                <!--date -->
+                <label class="label" for="date">Datum</label>
+                <input class="input is-link" id="date" type="date" name="date"
+                       value="<?= date('Y-m-d') ?>"/>
+
+                <!-- Submit -->
+
+                <button class="button is-link is-outlined is-fullwidth">Kies een Datum
+                </button>
+            </form>
+
+        </section>
+    </div>
+
+
+</main>
+
+<style>
+    .textArea {
+        border: antiquewhite solid 1px;
+        border-radius: 5px;
+        max-width: 656.73px;
+        background-color: #1C1512;
+        resize: none;
+    }
+
+    .inputDesign {
+
+    }
+</style>
+
+<footer class="has-background-info">
+    <div class="footerRow">
+        <div><p>Follow Us!</p>
+            <a href="https://www.instagram.com/broers.ridderkerk">
+                <i class="fa fa-instagram" style="font-size:30px"></i>
+            </a>
+
+            <a href="https://www.facebook.com/profile.php?id=61562490741128">
+                <i class="fa fa-facebook-square" style="font-size:30px"></i>
+            </a>
+        </div>
+        <div>
+            <div class="openingstijdenOnder"><h3>Openingstijden</h3>
+                <div class="openingstijdenSamen">
+                    <div class="openingstijden"><p>Woensdag</p>
+                        <p>Donderdag</p>
+                        <p>Vrijdag</p>
+                        <p>Zaterdag</p>
+                        <p>Zondag</p></div>
+                    <div class="openingstijden"><p>11:00-23:00</p>
+                        <p>11:00-23:00</p>
+                        <p>11:00-23:00</p>
+                        <p>11:00-23:00</p>
+                        <p>12:00-22:00</p></div>
+                </div>
+            </div>
+        </div>
+        <div class="detailsFooter"><img class="broersLogo" src="../../../Images/BroersLogo.png" alt="logo">
+            <p>Eetcafé BROERS</p>
+            <p>Koningsplein 44</p>
+            <p>2981 EA Ridderkerk</p>
+            <p>info@broersridderkerk.nl</p>
+            <p>06 - 48 18 54 03</p></div>
+    </div>
+</footer>
+
+</body>
