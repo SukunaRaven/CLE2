08a43e9837c0cbc72a28e731061291803b2d94af
Anemoia <killboy1100@gmail.com>
2025-01-21 09:57:42 +0100

    Fixed the Nav again

--- arrangementen.php
+++ arrangementen.php
@@ -23,8 +23,8 @@ session_start();
     <div class="navbar-brand">
         <a class="navbar-item">
             <figure class="image is-32x32">
-                <a href="../homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
-                                                              alt="Logo"/></a>
+                <a href="homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
+                                                           alt="Logo"/></a>
             </figure>
         </a>
 
@@ -57,15 +57,15 @@ session_start();
 
     <div class="navbar-menu" id="nav-bar">
         <div class="navbar-end">
-            <a href="../menu.php" class="navbar-item is-link">
+            <a href="menu.php" class="navbar-item is-link">
                 Ons Menu
             </a>
 
-            <a href="../reserveringen.php" class="navbar-item is-link">
+            <a href="reserveringen.php" class="navbar-item is-link">
                 Reserveer
             </a>
 
-            <a href="../contact.php" class="navbar-item is-link">
+            <a href="contact.php" class="navbar-item is-link">
                 Contact
             </a>
 
@@ -75,16 +75,16 @@ session_start();
                 </a>
 
                 <div class="navbar-dropdown">
-                    <a href="../aanbod.php" class="navbar-item">
+                    <a href="aanbod.php" class="navbar-item">
                         Ons Aanbod
                     </a>
-                    <a href="../evenementen.php" class="navbar-item">
+                    <a href="evenementen.php" class="navbar-item">
                         Onze Evenementen
                     </a>
-                    <a href="../arrangementen.php" class="navbar-item">
+                    <a href="arrangementen.php" class="navbar-item">
                         Onze Arrangementen
                     </a>
-                    <a href="../reviewspage.php" class="navbar-item">
+                    <a href="reviewspage.php" class="navbar-item">
                         Reviews
                     </a>
                 </div>
@@ -92,21 +92,21 @@ session_start();
         </div>
         <?php if (empty($_SESSION)) : ?>
             <div class="buttons">
-                <a href="../login.php" class="button is-primary is-outlined">
+                <a href="login.php" class="button is-primary is-outlined">
                     Log In
                 </a>
             </div>
         <?php elseif (isset($_SESSION['admin_flag'])): ?>
             <?php if ($_SESSION['admin_flag'] == 1): ?>
                 <div class="buttons">
-                    <a href="../admin.php" class="button is-primary is-outlined">
+                    <a href="admin.php" class="button is-primary is-outlined">
                         Admin
                     </a>
                 </div>
             <?php endif; ?>
         <?php else: ?>
             <div class="buttons">
-                <a href="../logout.php" class="button is-link is-outlined">
+                <a href="logout.php" class="button is-link is-outlined">
                     Log Out
                 </a>
             </div>
