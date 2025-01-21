a741eb0b97556a2739926163308e1e7907371419
Anemoia <killboy1100@gmail.com>
2025-01-21 10:03:01 +0100

    Nap

--- login.php
+++ login.php
@@ -103,8 +103,8 @@ if (isset($_POST['submit'])) {
     <div class="navbar-brand">
         <a class="navbar-item">
             <figure class="image is-32x32">
-                <a href="../homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
-                                                              alt="Logo"/></a>
+                <a href="homepage.php" target="_self"><img class="is-rounded" src="../Images/BroersLogo.png"
+                                                           alt="Logo"/></a>
             </figure>
         </a>
 
@@ -137,19 +137,19 @@ if (isset($_POST['submit'])) {
 
     <div class="navbar-menu" id="nav-bar">
         <div class="navbar-end">
-            <a href="../menu.php" class="navbar-item is-link">
+            <a href="menu.php" class="navbar-item is-link">
                 Ons Menu
             </a>
 
-            <a href="../reserveringen.php" class="navbar-item is-link">
+            <a href="reserveringen.php" class="navbar-item is-link">
                 Reserveer
             </a>
 
-            <a href="../contact.php" class="navbar-item is-link">
+            <a href="contact.php" class="navbar-item is-link">
                 Contact
             </a>
 
-            <a href="../users.php" class="navbar-item is-link">
+            <a href="users.php" class="navbar-item is-link">
                 Users
             </a>
 
@@ -159,16 +159,16 @@ if (isset($_POST['submit'])) {
                 </a>
 
                 <div class="navbar-dropdown">
-                    <a href="../aanbod.php" class="navbar-item">
+                    <a href="aanbod.php" class="navbar-item">
                         Ons Aanbod
                     </a>
-                    <a href="../evenementen.php" class="navbar-item">
+                    <a href="evenementen.php" class="navbar-item">
                         Onze Evenementen
                     </a>
-                    <a href="../arrangementen.php" class="navbar-item">
+                    <a href="arrangementen.php" class="navbar-item">
                         Onze Arrangementen
                     </a>
-                    <a href="../reviewspage.php" class="navbar-item">
+                    <a href="reviewspage.php" class="navbar-item">
                         Reviews
                     </a>
                 </div>
@@ -176,21 +176,21 @@ if (isset($_POST['submit'])) {
         </div>
         <?php if (empty($_SESSION)) : ?>
             <div class="buttons">
-                <a href="../login.php" class="button is-primary is-outlined">
+                <a href="login.php" class="button is-primary is-outlined">
                     Log In
                 </a>
             </div>
         <?php elseif (isset($_SESSION['admin_flag'])): ?>
             <?php if ($_SESSION['admin_flag'] == 1): ?>
                 <div class="buttons">
-                    <a href="../admin.php" class="button is-primary is-outlined">
+                    <a href="admin.php" class="button is-primary is-outlined">
                         Admin
                     </a>
                 </div>
             <?php endif; ?>
         <?php else: ?>
             <div class="buttons">
-                <a href="../logout.php" class="button is-link is-outlined">
+                <a href="logout.php" class="button is-link is-outlined">
                     Log Out
                 </a>
             </div>
