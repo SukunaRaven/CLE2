25c0591e15a7fe4da3b1cfd6c6005b0ff6b474b8
lisa-mao <1091575@hr.nl>
2025-01-22 13:19:30 +0100

    agenda fix

--- connection/connection.php
+++ connection/connection.php
@@ -1,8 +1,13 @@
 <?php
+//$host = "127.0.0.1";
+//$database = "broers_db";
+//$user = "root";
+//$password = "";
+
 $host = "127.0.0.1";
-$database = "broers_db";
-$user = "root";
-$password = "";
+$database = "1091575";
+$user = "1091575";
+$password = "aithaeji";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
