ef179f0bf6572bcfbddbac51f76d2050312339be
SukunaRaven <sabrinagenegel@gmail.com>
2025-01-23 19:35:45 +0100

    Updates

--- connection/connection.php
+++ connection/connection.php
@@ -1,13 +1,13 @@
 <?php
-$host = "127.0.0.1";
-$database = "cle2";
-$user = "root";
-$password = "";
-
 //$host = "127.0.0.1";
-//$database = "1091575";
-//$user = "1091575";
-//$password = "aithaeji";
+//$database = "cle2";
+//$user = "root";
+//$password = "";
+
+$host = "127.0.0.1";
+$database = "1091575";
+$user = "1091575";
+$password = "aithaeji";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
